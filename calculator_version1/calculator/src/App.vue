312d993388827661290af8dca203e0b61df6040f
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-19 16:08:27 +0700

    adding_button

---
+++ calculator_version1/calculator/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-dark bg-grey">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+    <HelloWorld msg="Welcome Mony"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
+}
+</style>
