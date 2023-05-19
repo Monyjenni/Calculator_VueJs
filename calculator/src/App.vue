21e38dfafa70c1ab9a16e1b5ba6796fb2ed5cee0
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-19 15:03:59 +0700

    changes

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Calculator</h1>
   </div>
 </template>
 
@@ -19,10 +18,8 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 50%;
+  color:blue;
+  
 }
 </style>
