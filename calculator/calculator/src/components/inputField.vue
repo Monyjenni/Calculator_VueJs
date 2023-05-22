61554f444232f8d52bc34489e3ab24a1dc4637c9
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-05-22 15:17:13 +0700

    adding_input_twobinding

---
+++ calculator/calculator/src/components/inputField.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="body-col">
+        <div class="num-input-col-1">
+        <input type="number" v-model.number="num1"  id="myInput1">
+      </div>
+      <div class="num-input-col-2">
+        <input type="number" v-model.number="num2"  id="myInput2">
+      </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'inputField',
+    data () {
+        return {
+            num1: 0,
+            num2: 0,
+        }
+    }
+}
+</script>
+
+<style>
+.body-col {
+    padding-top: 20px;
+    display: flex;
+    justify-content: center;
+    justify-content: space-between;
+    margin: 10px 10px 10px 10px;
+   
+  }</style>
---
+++ calculator/calculator/src/components/operatorCal.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="operator-calculator">
+        <button class="btn btn-warning" @click="add" id="Op1">+</button>
+        <button class="btn btn-warning" @click="substract" id="Op2">-</button>
+        <button class="btn btn-warning" @click="multiply" id="Op3">*</button>
+        <button class="btn btn-warning" @click="divide" id="Op4">/</button>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: 'operatorCalculator'
+    }
+</script>
+
+<style scoped>
+
+</style>
